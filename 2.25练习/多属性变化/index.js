78bca602abf27ad2fc0d0de1d85f370d8782a54e
loop <17798271235@163.com>
2024-02-28 16:29:53 +0800

    master

--- 2.25练习/多属性变化/index.js
+++ 2.25练习/多属性变化/index.js
@@ -5,20 +5,29 @@ export default fn
  * @param {*} dom 
  * @param {*} style 
  */
-function fn(dom,style){
-    let timer;
-    timer = setInterval(function(){
-        for(let key in style){
-            let current = parseFloat(getStyle(dom, key))
-            let Mathfn = style[key]-current>0?'ceil':'floor'
-            let speed = Math[Mathfn]((style[key]-current)*0.05);
-            if(current == style[key]){
-                delete style[key];
-                if(Object.keys(style).length == 0)
-                clearInterval(timer)
-            }else{
-                dom.style[key] = current + speed + 'px'
+function fn(dom, targets, callback) {
+    let timer = setInterval(() => {
+        for (let key in targets) {
+
+            let current = key == 'opacity' ? parseFloat(getStyle(dom, key)) * 1000 : parseFloat(getStyle(dom, key));
+            // 计算此次移动距离吧
+            let target = key == 'opacity' ? targets[key] * 1000 : targets[key]
+            let method = target - current > 0 ? 'ceil' : 'floor';
+            let speed = Math[method]((target - current) * 0.05);
+            if (current == target) {
+                delete targets[key];
+                if (Object.keys(targets).length == 0) {
+                    clearInterval(timer);
+                }
+                console.log('over');
+                Object.prototype.toString.call(callback) == '[object Function]' && callback();
+            } else {
+                if (key == 'opacity') {
+                    dom.style[key] = (current + speed) / 1000;
+                } else {
+                    dom.style[key] = current + speed + 'px';
+                }
             }
         }
-    },20)
+    }, 20)
 }
